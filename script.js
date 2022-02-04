845d178d028cc41d878d954a34cf4d330c35ebf4
Cameron Chandler <webstormcam@gmail.com>
2022-02-04 05:44:04 -0500

    Put in the js code with the timer

---
+++ script.js
@@ -0,0 +1,35 @@
+let day = document.getElementById('dayNum')
+let hour = document.getElementById('hourNum')
+let minute = document.getElementById('minNum')
+let second = document.getElementById('secNum')
+
+var countDownDate = new Date("Jan 31, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+ day.innerHTML = days 
+ hour.innerHTML = hours
+ minute.innerHTML = minutes
+ second.innerHTML = seconds
+
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
